f46890b9b48a94bc01266db85f90af031c0e830f
devcharles <charlesipn@gmail.com>
2018-10-31 04:04:58 -0600

    deleteDir in fs

--- modulo_dos/fs/deleteDir.js
+++ modulo_dos/fs/deleteDir.js
@@ -1,6 +1,13 @@
+
 const fs = require('fs')
 
-fs.rmdir('./newDir', error => {
-  if (error) return console.error('Error: ', error)
-  console.log('Carpeta borrada sin problemas')
-})
+const dirContent = fs.readdirSync('./folder-to-delete/')
+const isEmptyDir = !dirContent[0]
+
+if (!isEmptyDir){
+  console.log('Voy a borrar archivos')
+  dirContent.forEach(file => fs.unlinkSync(`./folder-to-delete/${file}`))
+} else {
+  console.log('no voy a borrar archivos')
+}
+fs.rmdirSync('./folder-to-delete/')
