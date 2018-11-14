df8f6c1351eee543af06a8a10a861e3b0ae8ae5f
devcharles <charlesipn@gmail.com>
2018-11-13 19:44:40 -0600

    dish router added

---
+++ modulo_dos/fondAPI/src/routes/dishes.js
@@ -0,0 +1,62 @@
+const express = require('express')
+
+const router = express.Router()
+
+const dish = require('../usecases/dishes')
+
+router.get('/', async (req, res) => {
+  const dishes = await dish.get()
+  res.json({
+    success: true,
+    message: 'Done!',
+    payload: {
+      dishes
+    }
+  })
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const dishData = req.body
+    console.warn(' dishData: ', dishData)
+    const newDish = await dish.create(dishData)
+    res.json({
+      success: true,
+      message: 'New dish created',
+      payload: { dish: newDish }
+    })
+  } catch (error) {
+    res.status(400)
+    res.json({
+      success: false,
+      message: 'Dish could not be created',
+      error: [
+        error
+      ]
+    })
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const dishDeleted = await dish.del(id)
+
+    res.json({
+      success: true,
+      message: 'Dish deleted',
+      payload: { dish: dishDeleted }
+    })
+  } catch (error) {
+    res.status(400)
+    res.json({
+      success: false,
+      message: 'Dish could not be deleted',
+      error: [
+        error
+      ]
+    })
+  }
+})
+
+module.exports = router
--- modulo_dos/fondAPI/src/server.js
+++ modulo_dos/fondAPI/src/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 
-const dish = require('./usecases/dishes')
-
 const app = express()
 
+const dishesRoutes = require('./routes/dishes')
+
 app.use(express.json())
+
+app.use('/dishes', dishesRoutes)
 
 app.get('/', (req, res) => {
   orders.get()
@@ -12,62 +14,6 @@ app.get('/', (req, res) => {
     success: true,
     message: 'FondAPI running'
   })
-})
-
-app.get('/dishes', async (req, res) => {
-  const dishes = await dish.get()
-  res.json({
-    success: true,
-    message: 'Done!',
-    payload: {
-      dishes
-    }
-  })
-})
-
-app.post('/dishes', async (req, res) => {
-  try {
-    const dishData = req.body
-    console.warn(' dishData: ', dishData)
-    const newDish = await dish.create(dishData)
-    res.json({
-      success: true,
-      message: 'New dish created',
-      payload: { dish: newDish }
-    })
-  } catch (error) {
-    res.status(400)
-    res.json({
-      success: false,
-      message: 'Dish could not be created',
-      error: [
-        error
-      ]
-    })
-  }
-
-})
-
-app.delete('/dishes/:id', async (req, res) => {
-  try {
-    const { id } = req.params
-    const dishDeleted = await dish.del(id)
-
-    res.json({
-      success: true,
-      message: 'Dish deleted',
-      payload: { dish: dishDeleted }
-    })
-  } catch (error) {
-    res.status(400)
-    res.json({
-      success: false,
-      message: 'Dish could not be deleted',
-      error: [
-        error
-      ]
-    })
-  }
 })
 
 module.exports = app
