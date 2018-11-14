bbc127576c908906b35741e543c7570eb608808d
Daniel Ortega Escandón <danieloe-1994@hotmail.com>
2018-11-13 19:48:57 -0600

    Entrar

--- modulo_dos/fondAPI/index.js
+++
@@ -1,9 +0,0 @@
-const server = require('./src/server')
-const db = require('./src/lib/db')
-
-server.listen(8080, () => {
-  console.log('FondAPI running in port 8080')
-  db.connect()
-    .then(() => console.log('DB connected'))
-    .catch(() => console.error('DB error while connect'))
-})
