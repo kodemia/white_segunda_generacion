b93cddf23b48223b08cd74b3d75d10a289d3e7d3
charles <charlesipn@gmail.com>
2018-11-20 20:13:58 -0600

    jwt lib

--- modulo_dos/fondAPI/src/lib/jwt.js
+++ modulo_dos/fondAPI/src/lib/jwt.js
@@ -1,10 +1,10 @@
 const jwt = require('jsonwebtoken')
 
-const hash = process.env.JWT_KEY || 'superSecretKey'
-const ttl = process.env.JWT_TTL || '8h'
+const key = 'superSecretKey'
+const ttl = '8h'
 
 module.exports = {
-  create (data, secret = hash) {
+  create (data, secret = key) {
     return new Promise((resolve, reject) => {
       jwt.sign(data, secret, { expiresIn: ttl }, function (error, token) {
         if (error) return reject(error)
@@ -13,7 +13,7 @@ module.exports = {
     })
   },
 
-  verify (token, secret = hash) {
+  verify (token, secret = key) {
     return new Promise((resolve, reject) => {
       jwt.verify(token, secret, function (error, decoded) {
         if (error) return reject(error)
